6dad922e6a7c6b06ff95e49506478c3400a70947
Eric Walker <eric.walker@gmail.com>
2023-01-20 21:20:53 -0700

    Day 1

--- day1/pass1/src/main.rs
+++ day1/pass1/src/main.rs
@@ -1,8 +1,118 @@
-use std::io::{self, BufRead};
+#[macro_use]
+extern crate quick_error;
 
-fn main() {
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        println!("{}", line.unwrap());
+use std::{io, num::ParseIntError};
+
+quick_error! {
+    #[derive(Debug)]
+    pub enum Error {
+        IO(err: std::io::Error) {
+            from()
+        }
+
+        ParseInt(err: ParseIntError) {
+            from()
+        }
+    }
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+struct Elf {
+    pub calories: Vec<u16>,
+}
+
+impl Elf {
+    fn new() -> Self {
+        Self { calories: vec![] }
+    }
+
+    fn total_calories(&self) -> u16 {
+        self.calories.iter().sum()
+    }
+}
+
+struct Runner {
+    _input: Vec<String>,
+    pub elves: Vec<Elf>,
+}
+
+impl Runner {
+    fn parse(input: Vec<String>) -> Result<Self> {
+        let mut elves = vec![Elf::new()];
+
+        for line in input.iter() {
+            if line.is_empty() {
+                elves.push(Elf::new());
+                continue;
+            }
+
+            let calorie = line.parse::<u16>()?;
+            if let Some(elf) = elves.last_mut() {
+                elf.calories.push(calorie);
+            }
+        }
+
+        Ok(Self { _input: input, elves })
+    }
+
+    fn run(&self) -> u16 {
+        self.elves
+            .iter()
+            .map(|e| e.total_calories())
+            .max()
+            .unwrap_or_default()
+    }
+}
+
+fn main() -> Result<()> {
+    let mut lines: Vec<String> = vec![];
+
+    for line in io::stdin().lines() {
+        lines.push(line?);
+    }
+
+    let runner = Runner::parse(lines)?;
+    let max = runner.run();
+
+    println!("max calories: {}", max);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::str::FromStr;
+
+    #[test]
+    fn parse_input() {
+        let input: Vec<String> = "
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000
+        "
+        .trim()
+        .lines()
+        .map(|s| s.trim())
+        .map(|s| String::from_str(s).unwrap())
+        .collect::<Vec<String>>();
+
+        let runner = Runner::parse(input).unwrap();
+
+        assert_eq!(runner.elves.len(), 5);
+        assert_eq!(runner.run(), 24000);
     }
 }
