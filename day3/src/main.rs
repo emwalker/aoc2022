21d1203a4aed8328d58502e66bafb8b0feb7b594
Eric Walker <eric.walker@gmail.com>
2023-01-25 19:17:29 -0700

    Consolidate Day 3, parts 1 and 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -13,125 +13,103 @@ fn priority(c: char) -> Result<i32> {
     }
 }
 
-pub(crate) mod part1 {
-    use super::*;
+#[derive(Clone, Debug)]
+struct Rucksack {
+    pub shared_item: char,
+    pub counts: Counter<char>,
+}
 
-    pub struct Rucksack {
-        pub shared_item: char,
-    }
+impl FromStr for Rucksack {
+    type Err = Report;
 
-    impl FromStr for Rucksack {
-        type Err = Report;
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let s = s.trim();
+        let len = s.len();
 
-        fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
-            let s = s.trim();
-            let len = s.len();
-
-            if len % 2 != 0 {
-                return Err(eyre!("an even sized string is required"));
-            }
-
-            fn count(t: &str) -> Counter<char> {
-                t.chars().collect::<Counter<_>>()
-            }
-
-            let len = len / 2;
-            let (left, right) = (count(&s[..len]), count(&s[len..]));
-            let shared = left & right;
-
-            if let Some(&shared_item) = shared.keys().next() {
-                return Ok(Self { shared_item });
-            }
-
-            Err(eyre!("no item found more than once"))
-        }
-    }
-
-    pub fn calculate(lines: &[String]) -> Result<i32> {
-        let ruckacks: Vec<Rucksack> = lines
-            .iter()
-            .map(|l| l.parse::<Rucksack>())
-            .collect::<Result<Vec<Rucksack>>>()?;
-
-        let mut total = 0;
-        for r in ruckacks.iter() {
-            total += priority(r.shared_item)?;
+        if len % 2 != 0 {
+            return Err(eyre!("an even sized string is required"));
         }
 
-        Ok(total)
+        fn count(t: &str) -> Counter<char> {
+            t.chars().collect::<Counter<_>>()
+        }
+
+        let len = len / 2;
+        let (left, right) = (count(&s[..len]), count(&s[len..]));
+        let shared = left & right;
+
+        let counts = s.chars().collect::<Counter<_>>();
+
+        if let Some(&shared_item) = shared.keys().next() {
+            return Ok(Self {
+                shared_item,
+                counts,
+            });
+        }
+
+        Err(eyre!("no item found more than once"))
     }
 }
 
-pub(crate) mod part2 {
-    use super::*;
+pub struct Group {
+    rucksacks: [Rucksack; 3],
+}
 
-    pub struct Rucksack {
-        pub counts: Counter<char>,
+impl Group {
+    fn new(rucksacks: &[Rucksack]) -> Result<Self> {
+        let rucksacks: [Rucksack; 3] = rucksacks
+            .to_vec()
+            .try_into()
+            .map_err(|_err| eyre!("wrong number of lines"))?;
+        Ok(Self { rucksacks })
     }
 
-    impl FromStr for Rucksack {
-        type Err = Report;
+    fn badge(&self) -> Result<char> {
+        let c1 = self.rucksacks[0].counts.clone();
+        let c2 = self.rucksacks[1].counts.clone();
+        let c3 = self.rucksacks[2].counts.clone();
+        let shared = c1 & c2 & c3;
 
-        fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
-            let s = s.trim();
-            Ok(Self {
-                counts: s.chars().collect::<Counter<_>>(),
-            })
+        if let Some(&badge) = shared.keys().next() {
+            return Ok(badge);
         }
+
+        Err(eyre!("no badge found"))
     }
 
-    pub struct Group {
-        rucksacks: [Rucksack; 3],
+    pub fn priority(&self) -> Result<i32> {
+        if let Ok(b) = self.badge() {
+            return priority(b);
+        }
+
+        Err(eyre!("no badge"))
     }
+}
 
-    impl Group {
-        pub fn parse(lines: &[String]) -> Result<Self> {
-            let rucksacks: [Rucksack; 3] = lines
-                .iter()
-                .map(|l| l.parse::<Rucksack>())
-                .collect::<Result<Vec<Rucksack>>>()?
-                .try_into()
-                .map_err(|_err| eyre!("wrong number of lines"))?;
+struct Calculations {
+    part1: i32,
+    part2: i32,
+}
 
-            Ok(Self { rucksacks })
-        }
-
-        fn badge(&self) -> Result<char> {
-            let c1 = self.rucksacks[0].counts.clone();
-            let c2 = self.rucksacks[1].counts.clone();
-            let c3 = self.rucksacks[2].counts.clone();
-            let shared = c1 & c2 & c3;
-
-            if let Some(&badge) = shared.keys().next() {
-                return Ok(badge);
-            }
-
-            Err(eyre!("no badge found"))
-        }
-
-        pub fn priority(&self) -> Result<i32> {
-            if let Ok(b) = self.badge() {
-                return priority(b);
-            }
-
-            Err(eyre!("no badge"))
-        }
-    }
-
-    pub fn calculate(lines: &[String]) -> Result<i32> {
-        let mut total = 0;
+impl Calculations {
+    fn new(lines: &[String]) -> Result<Self> {
         let mut group = vec![];
+        let mut part1 = 0;
+        let mut part2 = 0;
 
         for line in lines.iter() {
-            group.push(line.to_owned());
+            let r = line.parse::<Rucksack>()?;
+            part1 += priority(r.shared_item)?;
+
+            group.push(r);
 
             if group.len() == 3 {
-                total += Group::parse(&group)?.priority()?;
+                part2 += Group::new(&group)?.priority()?;
                 group.clear();
             }
         }
 
-        Ok(total)
+        Ok(Calculations { part1, part2 })
     }
 }
 
@@ -144,13 +122,9 @@ fn main() -> Result<()> {
         .map(|l| l.unwrap())
         .collect::<Vec<String>>();
 
-    let total = part1::calculate(&lines)?;
-    println!("part 1: {total}");
-
-    match part2::calculate(&lines) {
-        Ok(total) => println!("part 2: {total}"),
-        Err(_) => println!("part 2 could not be calculated"),
-    };
+    let calcs = Calculations::new(&lines)?;
+    println!("part 1: {}", calcs.part1);
+    println!("part 1: {}", calcs.part2);
 
     Ok(())
 }
@@ -167,55 +141,49 @@ mod tests {
         assert_eq!(priority('Z').unwrap(), 52);
     }
 
-    mod part1_test {
-        use super::*;
+    #[test]
+    fn rucksack_from_str() {
+        let input = "\
+        vJrwpWtwJgWrhcsFMMfFFhFp
+        jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+        PmmdzqPrVvPwwTWBwg
+        wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+        ttgJtRGJQctTZtZT
+        CrZsJsPPZsGzwwsLwLmpwMDw";
 
-        #[test]
-        fn from_str() {
-            let input = "\
-            vJrwpWtwJgWrhcsFMMfFFhFp
-            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-            PmmdzqPrVvPwwTWBwg
-            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-            ttgJtRGJQctTZtZT
-            CrZsJsPPZsGzwwsLwLmpwMDw";
+        let actual = input
+            .lines()
+            .map(|s| s.parse::<Rucksack>().unwrap().shared_item)
+            .collect::<Vec<char>>();
+        let expected = ['p', 'L', 'P', 'v', 't', 's'];
 
-            let actual = input
-                .lines()
-                .map(|s| s.parse::<part1::Rucksack>().unwrap().shared_item)
-                .collect::<Vec<char>>();
-            let expected = ['p', 'L', 'P', 'v', 't', 's'];
-
-            assert_eq!(actual, expected);
-        }
+        assert_eq!(actual, expected);
     }
 
-    mod part2_test {
-        use super::*;
-
-        #[test]
-        fn from_str() {
-            let lines: Vec<String> = "\
+    #[test]
+    fn group_from_rucksacs() {
+        let rucksacks = "\
             vJrwpWtwJgWrhcsFMMfFFhFp
             jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
             PmmdzqPrVvPwwTWBwg"
-                .lines()
-                .map(String::from)
-                .collect();
+            .lines()
+            .map(|l| l.parse::<Rucksack>())
+            .collect::<Result<Vec<Rucksack>>>()
+            .unwrap();
 
-            let g = part2::Group::parse(&lines).unwrap();
-            assert_eq!(g.priority().unwrap(), 18);
+        let g = Group::new(&rucksacks).unwrap();
+        assert_eq!(g.priority().unwrap(), 18);
 
-            let lines: Vec<String> = "\
+        let rucksacks = "\
             wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
             ttgJtRGJQctTZtZT
             CrZsJsPPZsGzwwsLwLmpwMDw"
-                .lines()
-                .map(String::from)
-                .collect();
+            .lines()
+            .map(|l| l.parse::<Rucksack>())
+            .collect::<Result<Vec<Rucksack>>>()
+            .unwrap();
 
-            let g = part2::Group::parse(&lines).unwrap();
-            assert_eq!(g.priority().unwrap(), 52);
-        }
+        let g = Group::new(&rucksacks).unwrap();
+        assert_eq!(g.priority().unwrap(), 52);
     }
 }
